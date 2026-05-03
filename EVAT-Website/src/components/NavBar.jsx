17415fea1dbd9f606663f3d8b97e89b7cd6654a3
Paul Dal Pozzo <s214527809@deakin.edu.au>
2026-05-03 13:24:52 +1000

    Navbar update
    Updated the navigation bar to something more modern.
    The app title now navigates to the map. The profile pic navigates to the profile. The sign out button is in the top right corner and can be used on any page now.
    There is also a developer only button. It only appears in developer mode. This will allow us to add use case pages for testing without flooding UI elements.

--- EVAT-Website/src/components/NavBar.jsx
+++ EVAT-Website/src/components/NavBar.jsx
@@ -1,57 +1,136 @@
+
+import React, { useState, useEffect, useRef } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import logo from '../assets/logo.png';
 import profileImage from '../assets/profileImage.png';
+import { Menu, LogOut} from "lucide-react";
 
 function NavBar() {
     const navigate = useNavigate();
     const location = useLocation();
+    const [mainMenu, setMainMenuOpen] = useState(false);
+    const [devMenu, setDevMenuOpen] = useState(false);
+    const isDev = import.meta.env.DEV; // check if in dev mode
+
+    // Handle Sign out
+    const handleSignOut = () => {
+        localStorage.removeItem("currentUser");
+        navigate("/signin");
+    };
 
     // Highlight active button
     const isActive = (path) => location.pathname === path;
 
+    const toggleMainMenu = () => {
+        setMainMenuOpen(!mainMenu);
+        setDevMenuOpen(false);
+    }
+
+    const toggleDevMenu = () => {
+        setDevMenuOpen(!devMenu);
+        setMainMenuOpen(false);
+    }
+
     return (
         <nav className="navbar">
             <div className="left-navbar">
-                <img src={logo} alt="Logo" className="logo-navbar" />
-                <h5 className='title-navbar'>Electric Vehicle Adoption Tool</h5>
-            </div>
-            <div className="right-navbar">
-                <div>
+
+                <div className='dropdown'>
+                    {/* Main Menu Button */}
                     <button 
-                        className={`btn btn-navbar ${isActive('/profile') ? 'active' : ''}`} 
-                        onClick={() => navigate('/profile', { state: { resetDashboard: true } })}
-                    >My Dashboard</button>
-                    <button 
-                        className={`btn btn-navbar ${isActive('/map') ? 'active' : ''}`} 
-                        onClick={() => navigate('/map')}
-                    >Map</button>
-                    <button 
-                        className={`btn btn-navbar ${isActive('/favourites') ? 'active' : ''}`} 
-                        onClick={() => navigate('/favourites')}
-                    >Favourites</button>
-                    <button 
-                        className={`btn btn-navbar ${isActive('/game') ? 'active' : ''}`} 
-                        onClick={() => navigate('/game')}
-                    >Rewards</button>
-                    <button 
-                        className={`btn btn-navbar ${isActive('/cost') ? 'active' : ''}`} 
-                        onClick={() => navigate('/cost')}
-                    >Vehicle Analysis</button>
-                    <button 
-                        className={`btn btn-navbar ${isActive('/feedback') ? 'active' : ''}`} 
-                        onClick={() => navigate('/feedback')}
-                    >Feedback</button>
-                    <button 
-                        className={`btn btn-navbar ${isActive('/support') ? 'active' : ''}`} 
-                        onClick={() => navigate('/support')}
-                    >Contact Support</button>
+                        className='btn btn-navbar navbar-menu-option' 
+                        onClick={toggleMainMenu}
+                    >
+                        {<Menu />}
+                    </button>
+                    {/* Main Menu Options */}
+                    {mainMenu && (
+                        <div className={`dropdown-menu ${mainMenu ? 'show' : ''}`}>
+                            <button className='dropdown-menu-item' onClick={() => navigate('/profile')}>
+                                Profile
+                            </button>
+                            <button className='dropdown-menu-item' onClick={() => navigate('/map')}>
+                                Map
+                            </button>
+                            <button className='dropdown-menu-item' onClick={() => navigate('/cost')}>
+                                Cost Comparison
+                            </button>
+                            <button className='dropdown-menu-item' onClick={() => navigate('/favourites')}>
+                                Favourite Chargers
+                            </button>
+                            <button className='dropdown-menu-item' onClick={() => navigate('/game')}>
+                                Rewards
+                            </button>
+                            <hr></hr>
+                            <button className='dropdown-menu-item' onClick={() => navigate('/feedback')}>
+                                Feedback
+                            </button>
+                            <button className='dropdown-menu-item' onClick={() => navigate('/support')}>
+                                Support
+                            </button>
+                        </div>
+                    )}
+
+                    {/* ==================== DEVELOPER MENU ==================== */}
+                    {isDev && (
+                        <>
+                            {/* Developer Menu Button */}
+                            <button 
+                                className='btn btn-navbar navbar-menu-option' 
+                                onClick={toggleDevMenu}
+                            >
+                                Developer Pages
+                            </button>
+
+                            {/* Developer Menu Options */}
+                            {devMenu && (
+                                <div className={`dropdown-menu ${devMenu ? 'show' : ''}`}>
+                                    <button className='dropdown-menu-item' onClick={() => navigate('/apitester')}>
+                                        API Tester
+                                    </button>
+                                    <button className='dropdown-menu-item' onClick={() => navigate('/voice-query')}>
+                                        Voice Query
+                                    </button>
+                                </div>
+                            )}
+                        </>
+                    )}
+                    {/* ======================================================= */}
+
                 </div>
-                <img src={profileImage} alt="Profile" className="icon-navbar middle" />
+            </div>
+
+
+            {/* Center Navbar */}
+            <div className="center-navbar center" >
+                <button 
+                    // Change this navigation to /map when complete
+                    className={`btn-navbar `} 
+                    onClick={() => navigate('/map')}
+                >
+                    <img src={logo} alt="Logo" className="logo-navbar"/>
+                    <h5 className='title-navbar'>Electric Vehicle Adoption Tool</h5>
+                </button>
+            </div>
+
+
+            {/* Right Navbar */}
+            <div className="right-navbar">
+                <img src={profileImage} 
+                    alt="Profile" 
+                    className="icon-navbar middle" 
+                    onClick={() => navigate('/profile')}
+                />
+                <button 
+                    alt="Sign Out"
+                    className={`btn btn-navbar`} 
+                    onClick={handleSignOut}
+                ><LogOut/></button>
             </div>
         </nav>
-        
     );
 }
 
-
 export default NavBar;
+
+
